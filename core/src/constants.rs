4a7e6b1c60e0f12031f56c853464cccc115ef76e
fold2ev <252091682+fold2ev@users.noreply.github.com>
2025-12-16 11:09:33 +0900

    feat(const): define protocol constants

---
+++ core/src/constants.rs
@@ -0,0 +1,4 @@
+pub const MAX_DEPTH: u32 = 30;
+pub const MIN_BUFFER_SIZE: usize = 8;
+pub const CANOPY_LIMIT: usize = 17;
+pub const PROGRAM_ID: &str = "Fold111111111111111111111111111111111111111";
